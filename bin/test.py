372126769a5c694826a09343b7747b27c0a00579
Rafa <rbenrax@gmail.com>
2023-08-03 20:29:17 +0200

    Add files via upload

--- bin/test.py
+++ bin/test.py
@@ -1,1 +1,12 @@
 
+import time, sys, utls
+
+def __main__(args):
+    
+    print (f"Prueba de llamada {args}")
+    
+    print(sys.modules)
+    
+    print(utls.human(12274))
+
+    time.sleep(1)
