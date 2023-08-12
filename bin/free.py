868c3733a61f7025a65781f32272077f622242c3
Rafa <rbenrax@gmail.com>
2023-08-12 15:32:52 +0200

    Update free.py

--- bin/free.py
+++ bin/free.py
@@ -1,17 +1,14 @@
-import sys
-import sdata
 import gc
+import utls 
 
-"""Show ram status"""
 def __main__(args):
 
-    if len(args) == 1:
+    mode=""
+    if len(args) > 0:
         mode=args[0]
-    else:
-        mode=""
 
-    if mode == "-h":
-        print("df <options>, options -h -p")
+    if mode == "--h":
+        print("Show ram status, df <options>: --h -h -p")
         return
     
     gc.collect()
@@ -24,13 +21,16 @@ def __main__(args):
     if mode=="-p":
         d={"total": t, "alloc": a, "free": f, "%": p}
         print(d)
+    elif mode == "-h":
+        print(f'\033[0mTotal.:\033[1m {utls.human(t)}')
+        print(f'\033[0mAlloc.:\033[1m {utls.human(a)}')
+        print(f'\033[0mFree..:\033[1m {utls.human(f)} {p}\033[0m')
     else:
         print(f'\033[0mTotal.:\033[1m {t:7} bytes')
         print(f'\033[0mAlloc.:\033[1m {a:7} bytes')
         print(f'\033[0mFree..:\033[1m {f:7} bytes {p}\033[0m')
-
-    #del sys.modules["gc"]
     
-if __name__ == "__main__":      
-    __main__("-h")
+if __name__ == "__main__":
+    a=["-h"]
+    __main__(a)
         
