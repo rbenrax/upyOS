ec59f4b8b9542b466ccf80c1283d82751bf081b6
Rafa <rbenrax@gmail.com>
2023-08-14 13:23:36 +0200

    lineup

--- bin/lineup.py
+++ bin/lineup.py
@@ -5,7 +5,7 @@ def __main__(args):
     if len(args) == 0:
         print ("Usage:")
         print ("lineup <file>, upload file in line mode,")
-        print ("empty line to end upload")
+        print ("Send CTRL+D to end upload")
         return
     
     with open(args[0], "wt") as fp:
