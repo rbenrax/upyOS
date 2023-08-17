c0685508acce910027d2a78ce5c637fe8ba5116e
rafa <rbenrax@gmail.com>
2023-08-17 18:55:54 +0200

    sdata get gpiop

--- lib/sdata.py
+++ lib/sdata.py
@@ -9,9 +9,20 @@ sysconfig={}
 
 # ---
 
+#To local check
 #board={"i2c"   : [{"scl": 27, "sda": 28}],
-#       "gpio"  : [{0: 2, 1: 3, 2: 19, 3: 20, 4: 28, 5: 27, 6: 22, 7: 23, 8: 29, 9: 30, 10: 21, 11: 24, 12: 04, 13: 10, 18: 5, 19: 6 }]}
-#TODO: check
+#       "gpio"  : [{"0": 2, "1": 3, "2": 19, "3": 20, "4": 28, "5": 27, "6": 22, "7": 23, "8": 29, "9": 30, "10": 21, "11": 24, "12": 04, "13": 10, "18": 5, "19": 6 }]}
+
+
+"""Get the gpio assigned to a pin"""
+def getgpiop(pin):
+    gpios=board["gpio"][0]
+    for k, v in gpios.items():
+        if v == pin:
+            return int(k)
+    return 0
+
+"""Get a dict of pins for a specific service category"""
 def getgpio(cat, ins):
     gps={}
     gpios=board["gpio"][0]
@@ -24,7 +35,10 @@ def getgpio(cat, ins):
                 #print(kps, k)
     return gps
 
+
 if __name__ == "__main__":
     
     gpios = getgpio("i2c", 0)
     print(gpios)
+    
+    print(getgpiop(4))
