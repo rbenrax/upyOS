fa34c9506318f21d4f5b7ac4d75c1dbc45b3607b
Rafa <rbenrax@gmail.com>
2023-08-19 15:21:40 +0200

    Update echo.py

--- bin/echo.py
+++ bin/echo.py
@@ -1,11 +1,17 @@
 import sdata
 
 def __main__(args):
-    if len(args) == 1:
-        val = sdata.getenv(args[0])
-        print(val)
-        return(val)
+    if len(args) > 1:
+        ret=""
+        for a in args:
+            val = sdata.getenv(a)
+            if val=="":
+                ret+=a + " "
+            else:
+                ret+=val + " "
+        print(ret[-1])
+        return(ret[-1])
     else:
-        print("Show env variable, echo <var>: var $?, $1, ..., any")
+        print("Show env variable, echo const/<var>: var $?, $1, ..., any")
 
 
