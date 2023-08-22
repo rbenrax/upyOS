7dd6721309b2d3f490e4a9669884dbc8a7417b7c
rafa <rbenrax@gmail.com>
2023-08-22 12:21:12 +0200

    init.rc

--- etc/init.sh
+++ etc/init.sh
@@ -8,14 +8,16 @@ py print("\033[0m",end="")
 py print("\\n")
 lshw -b
 
-py print("\\n\033[1mMemory:")
+py print("\\n"*2)
+py print("\033[1mMemory:")
 free
 
-py print("\\n\033[1mStorage:")
+py print("\\n"*2)
+py print("\033[1mStorage:")
 df
 
 led on 0             # Turn on led 0
-wait .1
+wait .05
 led off 0            # Turn off led 0
 
 #led rgb 1            # Test rgb led in board (if board has one)
