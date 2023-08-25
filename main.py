4b9d5733ce244f7c9ec8af8cf0f230dc5bd78b19
Rafa <rbenrax@gmail.com>
2023-08-25 10:43:30 +0200

    main

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-# by default python looks for modules in /lib directory
-# import sys
-# print(sys.path)
-
 # Launch smolOS with grub the first time to generate the /etc .board file
 # import grub
 # grub.mbr()
 
-# Launch smolOS once generated the .board file, aprox 0.6kb ram saved
+# Launch smolOS once generated the .board file, aprox 0.5kb ram saved
 import smolos
 smol = smolos.smolOS()
