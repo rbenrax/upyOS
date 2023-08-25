41b6e521243fe6ca688a7ad844eb5e4b8a5a434c
Rafa <rbenrax@gmail.com>
2023-08-25 11:58:20 +0200

    echo

--- bin/echo.py
+++ bin/echo.py
@@ -2,15 +2,9 @@ import sdata
 
 def __main__(args):
     if len(args) > 0:
-        ret=""
-        for a in args:
-            val = sdata.getenv(a)
-            if val=="":
-                ret += a + " "
-            else:
-                ret += val + " "
-                
-        print(ret[:-1])
-        return(ret[:-1])
+        for i, a in enumerate(args):
+            if a[0]=="$":
+                args[i] = sdata.getenv(a[1:])
+        print(" ".join(args))
     else:
         print("Show msg + env variable\nUsage: echo const/<var>: var $?, $1, ..., any")
