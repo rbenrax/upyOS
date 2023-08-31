65e4be5e0dff3aade384a307d0e8c9435de62905
rafa <rbenrax@gmail.com>
2023-08-31 12:26:45 +0200

    free

--- bin/free.py
+++ bin/free.py
@@ -1,4 +1,5 @@
 import gc
+import micropython
 import utls 
 
 def __main__(args):
@@ -17,18 +18,21 @@ def __main__(args):
     a = gc.mem_alloc()
     t = f+a
     p = f'({f/t*100:.2f}%)'
+    s = micropython.stack_use()
     
     if mode=="-p":
-        d={"total": t, "alloc": a, "free": f, "%": p}
+        d={"total": t, "alloc": a, "free": f, "%": p, "s": s}
         print(d)
     elif mode == "-h":
-        print(f'\033[0mTotal.:\033[1m {utls.human(t)}')
-        print(f'\033[0mAlloc.:\033[1m {utls.human(a)}')
-        print(f'\033[0mFree..:\033[1m {utls.human(f)} {p}\033[0m')
+        print(f'\033[0mTotal:\033[1m {utls.human(t)}')
+        print(f'\033[0mAlloc:\033[1m {utls.human(a)}')
+        print(f'\033[0mFree.:\033[1m {utls.human(f)} {p}\033[0m')
+        print(f'\033[0mStack:\033[1m {utls.human(s)} {p}\033[0m')
     else:
-        print(f'\033[0mTotal.:\033[1m {t:7} bytes')
-        print(f'\033[0mAlloc.:\033[1m {a:7} bytes')
-        print(f'\033[0mFree..:\033[1m {f:7} bytes {p}\033[0m')
+        print(f'\033[0mTotal:\033[1m {t:7} bytes')
+        print(f'\033[0mAlloc:\033[1m {a:7} bytes')
+        print(f'\033[0mFree.:\033[1m {f:7} bytes {p}\033[0m')
+        print(f'\033[0mStack:\033[1m {s:7} bytes')
     
 if __name__ == "__main__":
     a=["-h"]
