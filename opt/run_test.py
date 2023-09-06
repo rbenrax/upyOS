81283f4ac1a9ed864557a55c6d3ac82b61463553
Rafa <rbenrax@gmail.com>
2023-09-06 11:16:06 +0200

    run_test

--- opt/run_test.py
+++ opt/run_test.py
@@ -2,13 +2,7 @@ import sdata
 import sys
 
 def __main__(args):
-    print(f" local {args=}")
+    print(f"Hello World: {args=}")
 
-
-#print(dir())
-#print(args)
-
-#__main__(args) # only in run mode
-
-#if __name__ == '__main__':
-#    __main__("-f")
+if __name__ == '__main__':
+    __main__("a1 a 2 a3 a4 an")
