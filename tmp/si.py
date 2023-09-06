cb257ebf99d82cfdbe5ba2af30a63feffb57b6de
Rafa <rbenrax@gmail.com>
2023-09-06 19:23:23 +0200

    si

--- tmp/si.py
+++ tmp/si.py
@@ -1,4 +1,5 @@
-import gc, micropython
+# gc
+import gc
 gc.collect()
 f = gc.mem_free()
 a = gc.mem_alloc()
@@ -7,4 +8,24 @@ p = f'({f/t*100:.2f}%)'
 d={"total": t, "alloc": a, "free": f, "%": p}
 print(d)
 
-micropython.mem_info(1)
+# micropython
+import micropython
+micropython.mem_info()
+
+# platform
+import uplatform
+dir(uplatform)
+print(f"{uplatform.libc_ver()=}")
+print(f"{uplatform.platform()=}")
+print(f"{uplatform.python_compiler()=}")
+
+# sys
+import sys
+print(f"{sys.byteorder=}")
+print(f"{sys.implementation=}")
+print(f"{sys.maxsize=}")
+print(f"{sys.modules=}")
+print(f"{sys.path=}")
+print(f"{sys.platform=}")
+print(f"{sys.version=}")
+print(f"{sys.version_info=}")
