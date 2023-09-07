81e4b47a3b35b765ca06b5af3559aed2deb485af
Rafa <rbenrax@gmail.com>
2023-09-07 11:34:38 +0200

    run_test

--- opt/run_test.py
+++ opt/run_test.py
@@ -1,8 +1,15 @@
 import sdata
 import sys
 
+# The user space functions can avoid module being removed (passed in call)
+rmmod=False
+
+# The user space functions can call system funcions by syscall reference (passed in call)
+syscall=None
+
 def __main__(args):
     print(f"Hello World: {args=}")
+    syscall.print_msg(f"Hello World from syscall: {args=}")
 
 if __name__ == '__main__':
     __main__("a1 a 2 a3 a4 an")
