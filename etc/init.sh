bb0d2e76d819f68478805198b57040cd9dad29db
Rafa <rbenrax@gmail.com>
2023-09-12 10:09:10 +0200

    init.sh

--- etc/init.sh
+++ etc/init.sh
@@ -1,10 +1,11 @@
-# This script file is the first script called in boot, this is normal mode, if does not exists, 
+# This script file is the first script called in boot, this is normal mode, if does not exists,
 # we are in recovery mode and should exists init.rec
 # Internal and external commands can be used
 # > (Execute Python code)
 # < (Print the Python code execution)
 
-#clear
+cpufreq -turbo
+
 < "\033[1;33;44m", end=""
 cat /etc/wellcome.txt
 < "\033[0m", end=""
@@ -14,15 +15,15 @@ lshw -b
 
 < "\\n"*2
 < "\033[1mMemory:"
-free
+free -h
 
 < "\\n"*2
 < "\033[1mStorage:"
-df
+df -h
 
 led on 0             # Turn on led 0
 wait .05
 led off 0            # Turn off led 0
 
-#led rgb 1            # Test rgb led in board (if board has one)
+led rgb 1            # Test rgb led in board (if board has one)
 
