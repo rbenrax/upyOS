136db37efe1a99d270ffbfe1cb5ef4598b0891e5
Rafa <rbenrax@gmail.com>
2023-09-18 13:43:51 +0200

    ifconfig

--- bin/ifconfig.py
+++ bin/ifconfig.py
@@ -14,10 +14,9 @@ def __main__(args):
         print (f"      MAC: {mac2Str(nif.config("mac"))}")
         print (f"      DNS: {ic[3]}")
         print (f"      status: {'Active' if nif.active() else 'Inactive'}")
-        print (f"              {'Connected' if i == "sta" and nif.isconnected() else 'Disconnected'}")
-    
-    sta_if = network.WLAN(network.STA_IF)
-    pif("sta", sta_if)
-    
-    ap_if = network.WLAN(network.AP_IF)
-    pif("ap", ap_if)
+        if i=="sta":
+            print (f"              {'Connected' if i == "sta" and nif.isconnected() else 'Disconnected'}")
+
+    # TODO: enumerate nic
+    pif("sta", network.WLAN(network.STA_IF))
+    pif("ap", network.WLAN(network.AP_IF))
