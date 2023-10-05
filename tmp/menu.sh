2528d5b33bb6cdd4cf2d45cdc5346d3863d679c6
Rafa benavides <rbenrax@gmail.com>
2023-10-05 11:20:22 +0000

    menu.sh

--- tmp/menu.sh
+++ tmp/menu.sh
@@ -2,14 +2,14 @@
 clear
 < "Options Menu"
 < ""
-< "Option 1"
-< "Option 2"
+< "Option 1 160MHz"
+< "Option 2 240Mhz"
 < "Option 3 return"
 < "Option 0 exit"
 < ""
 echo "La opcion es: " $v1
 read v1 "Introduzca opcion: "
-if $v1 == 1 < "Opcion 1"
-if $v1 == 2 < "Opcion 2"
+if $v1 == 1 cpufreq 160
+if $v1 == 2 cpufreq 240
 if $v1 == 3 return
 if $v1 != 0 goto loop
