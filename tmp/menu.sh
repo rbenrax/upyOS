6a8a66c6c203f8a73d2f74e7f7eaf94f8028ade7
Rafa benavides <rbenrax@gmail.com>
2023-10-05 11:13:20 +0000

    menu.sh

--- tmp/menu.sh
+++ tmp/menu.sh
@@ -1,8 +1,10 @@
-clear
 :loop
-< "Opcion 1"
-< "Opcion 2"
-< "Opcion 0"
+clear
+< "Options Menu"
+< ""
+< "Option 1"
+< "Option 2"
+< "Option 0 reload"
 < ""
 echo "La opcion es: " $v1
 read v1 "Introduzca opcion: "
