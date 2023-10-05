5610ebd1a6f53820f0299ba677d9b42552a172c7
Rafa benavides <rbenrax@gmail.com>
2023-10-05 11:04:39 +0000

    labels & goto in scripts

--- bin/sh.py
+++ bin/sh.py
@@ -14,6 +14,93 @@ proc=None
 #    - Any program o command to run (ls, etc.)
 #    - An integer, the number of lines to skip, empty lines included
 #    - keyword: return, end script execution
+labels={}
+
+def run(ssf, label):
+    print(ssf)
+    line=0
+    with open(ssf,'r') as f:
+
+        skip_lines = 0
+            
+        while True:
+            lin = f.readline()
+            if not lin: break
+            line+=1
+
+            if label > 0 and line <= label: # Skip lines in goto
+                continue
+
+            if skip_lines > 0: # Skip lies in skip
+                skip_lines-=1
+                continue
+            
+            if lin.strip()=="": continue
+            if len(lin)>0 and lin[0]=="#": continue
+            cmdl=lin.split("#")[0] # Left part of commented line
+            
+            global labels
+            #print(cmdl)
+            if cmdl[0]==":":
+                labels[cmdl[1:-1]]=line # Save labels and his line
+                #print(labels)
+                continue
+            
+            # Unconditional commands
+            # End script
+            if cmdl[:6] =="return": break
+
+            # Skip lines
+            if cmdl[:5] =="skip ":
+                tmp = cmdl.split()
+                acca = tmp[1]
+                if acca.isdigit():
+                   skip_lines=int(acca)
+                   continue
+
+            # Conditional execution: if $0 == 5 return (ex.)
+            if cmdl[:3] =="if ":
+                tmp = cmdl.split()
+                # Translate env vars
+                for i, e in enumerate(tmp):
+                    if e[0]=="$":
+                        v=proc.syscall.getenv(e[1:])
+                        tmp[i] = v
+                        
+                arg1 = tmp[1] # operand1
+                op   = tmp[2] # operator
+                arg2 = tmp[3] # operand2
+                acc  = tmp[4] # action
+                
+                acca=""
+                if len(tmp) > 5:
+                    acca = tmp[5] # action arg
+                    
+                res = eval('"' + arg1 + '"' + op + '"' + arg2 + '"')
+                #if sdata.debug:
+                #    print(f"{line}: {cmdl[:-1]} {res=}")
+
+                if res:
+                    if acc == "goto":
+                        label=labels[acca]
+                        return label
+                       
+                    if acc == "return": break
+                    elif acc=="skip" and acca.isdigit():
+                        skip_lines=int(acca)
+                        continue
+                    elif acc=="run":
+                        proc.syscall.run_cmd(" ".join(tmp[5:]))
+                    else:
+                        #if sdata.debug:
+                        #    print(" ".join(tmp[4:]))
+                        proc.syscall.run_cmd(" ".join(tmp[4:]))
+                continue
+
+            proc.syscall.run_cmd(cmdl)
+            
+        return 0
+
 
 def __main__(args):
 
@@ -24,66 +111,14 @@ def __main__(args):
     ssf=args[0]
     
     # Run shell script
-    line=0
     if utls.file_exists(ssf):
-        with open(ssf,'r') as f:
-            skip_lines=0
-            while True:
-                lin = f.readline()
-                if not lin: break
-                line+=1
-
-                if skip_lines > 0:
-                    skip_lines-=1
-                    continue
-                
-                if lin.strip()=="": continue
-                if len(lin)>0 and lin[0]=="#": continue
-                cmdl=lin.split("#")[0] # Left part of commented line 
-                
-                # Unconditional commands
-                # End script
-                if cmdl[:6] =="return": break
-
-                # Skip lines
-                if cmdl[:5] =="skip ":
-                    tmp = cmdl.split()
-                    acca = tmp[1]
-                    if acca.isdigit():
-                       skip_lines=int(acca)
-                       continue
-
-                # Conditional execution: if $0 == 5 return (ex.)
-                if cmdl[:3] =="if ":
-                    tmp = cmdl.split()
-                    arg1 = tmp[1] # operand1
-                    op   = tmp[2] # operator
-                    arg2 = tmp[3] # operand2
-                    acc  = tmp[4] # action
-                    
-                    if len(tmp) > 5:
-                        acca = tmp[5] # action arg
-                    else:
-                        acca=""
-                        
-                    res = eval('"' + arg1 + '"' + op + '"' + arg2 + '"')
-                    #if sdata.debug:
-                    #    print(f"{line}: {cmdl[:-1]} {res=}")
-
-                    if res:
-                        if acc == "return": break
-                        elif acc=="skip" and acca.isdigit():
-                            skip_lines=int(acca)
-                            continue
-                        elif acc=="run":
-                            proc.syscall.run_cmd(" ".join(tmp[5:]))
-                        else:
-                            #if sdata.debug:
-                            #    print(" ".join(tmp[4:]))
-                            proc.syscall.run_cmd(" ".join(tmp[4:]))
-                    continue
-
-                proc.syscall.run_cmd(cmdl)
+        label=0
+        while True:
+            ret = run(ssf, label) # Call script once on every label
+            if ret==0:
+                break
+            else:
+                label=ret
     else:
         print(f"{ssf}: script not found")
 
