7510755d62bfc828aad593092a295277c6c74d42
Rafa benavides <rbenrax@gmail.com>
2023-10-05 11:18:05 +0000

    menu.sh

--- tmp/menu.sh
+++ tmp/menu.sh
@@ -4,10 +4,12 @@ clear
 < ""
 < "Option 1"
 < "Option 2"
-< "Option 0 reload"
+< "Option 3 return"
+< "Option 0 exit"
 < ""
 echo "La opcion es: " $v1
 read v1 "Introduzca opcion: "
 if $v1 == 1 < "Opcion 1"
 if $v1 == 2 < "Opcion 2"
+if $v1 == 3 return
 if $v1 != 0 goto loop
