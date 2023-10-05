7f149daeed10e3ff95ca238d695b92532a6abfa9
rafa <rbenrax@gmail.com>
2023-10-05 16:39:25 +0200

    adjs

--- etc/init.sh
+++ etc/init.sh
@@ -4,14 +4,14 @@
 # > (Execute Python code)
 # < (Print the Python code execuion)
 
-#cpufreq 160 # If no board configuration, this and others commands can fail and should not be used 
+#cpufreq 133 # If no board configuration, this and others commands can fail and should not be used 
 
 < "\033[1;33;44m", end=""
 cat /etc/wellcome.txt
 < "\033[0m", end=""
 
-#loadboard    # You can choose do not load or use another board configuration, without param default
-loadboard /etc/upyOS-esp32c3.board
+loadboard    # You can choose do not load or use another board configuration, without param default
+#loadboard /etc/upyOS-esp32c3.board
 #loadboard /etc/upyOS-esp32-wroom-32.board
 
 < "\\n"
