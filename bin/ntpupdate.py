869ce99d7bda8880eb897a4bb08134b2356a27d7
Rafa <rbenrax@gmil.com>
2023-10-08 19:05:47 +0200

    ntpupdate

--- bin/ntpupdate.py
+++ bin/ntpupdate.py
@@ -16,11 +16,12 @@ def __main__(args):
         
         ntptime.host = args[0]
         ntptime.settime()
+        #ntptime.settime(timezone=2, server = args[0]) # doesnt work
 
         from machine import RTC
         rtc = RTC()
         date_time = list(rtc.datetime())
-        date_time[4] = date_time[4] + offset
+        date_time[4] = date_time[4] + offset # TODO: not fine, looks for a better solution 
         date_time = tuple(date_time)
         rtc.datetime(date_time)
         
