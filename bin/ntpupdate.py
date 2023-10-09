5ed1eafe68afd56ac237511e2deea76f2f9c2c0f
Rafa <rbenrax@gmail.com>
2023-10-09 10:38:14 +0200

    ntpupdate ok

--- bin/ntpupdate.py
+++ bin/ntpupdate.py
@@ -19,11 +19,14 @@ def __main__(args):
         from machine import RTC
         rtc = RTC()
         
+        ## --- M1 (not fine)
         #date_time = list(rtc.datetime())
-        #date_time[4] = date_time[4] + offset # TODO: not fine, looks for a better solution
+        #date_time[4] = date_time[4] + offset 
         #date_time = tuple(date_time)
-        #print(f"P1{date_time}")
+        ##print(f"P1{date_time}")
+        #rtc.datetime(date_time)
         
+        ## --- M2
         y, mo, d, _, h, m, s, _ = rtc.datetime()
         
         # Calcular el desplazamiento en segundos
@@ -40,13 +43,17 @@ def __main__(args):
         ns = ns % 60
         
         # Configurar la nueva fecha y hora en el RTC
-        #print(f"P1{y} {m} {d}")
+        #print(f"P1{y} {mo} {d} {nh} {nm} {ns}")
         rtc.datetime((y, mo, d, 0, nh, nm, ns, 0))
+        
+        # --- M3 (Bad)
+        
+        #sec = utime.mktime(rtc.datetime())
+        #sec = utime.mktime(utime.localtime())
+        #sec = sec + (tz * 3600)
+        #y, mo, d, _, h, m, s, _ = utime.gmtime(sec)
+        ##print(f"P1: {y} {mo} {d} {h} {m} {s}")
+        #rtc.datetime((y, mo, d, 0, h, m, s, 0))
         
     except Exception as ex:
         print("ntpupdate error: ", ex)
-        
-        
-        
-
-        
