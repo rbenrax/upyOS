14e981867757fb4086e27628926030c80e0737ca
Rafa <rbenrax@gmail.com>
2023-10-09 09:23:46 +0200

    ntpupdate

--- bin/ntpupdate.py
+++ bin/ntpupdate.py
@@ -1,6 +1,5 @@
 import ntptime
-
-import utls
+import utime
 
 proc=None
 
@@ -12,18 +11,37 @@ def __main__(args):
     
     try:
         
-        offset = int(proc.syscall.getenv("TZ"))
+        tz = int(proc.syscall.getenv("TZ"))
         
         ntptime.host = args[0]
         ntptime.settime()
-        #ntptime.settime(timezone=2, server = args[0]) # doesnt work
 
         from machine import RTC
         rtc = RTC()
-        date_time = list(rtc.datetime())
-        date_time[4] = date_time[4] + offset # TODO: not fine, looks for a better solution 
-        date_time = tuple(date_time)
-        rtc.datetime(date_time)
+        
+        #date_time = list(rtc.datetime())
+        #date_time[4] = date_time[4] + offset # TODO: not fine, looks for a better solution
+        #date_time = tuple(date_time)
+        #print(f"P1{date_time}")
+        
+        y, mo, d, _, h, m, s, _ = rtc.datetime()
+        
+        # Calcular el desplazamiento en segundos
+        offset = tz * 3600
+        
+        # Sumar el desplazamiento a la hora actual
+        nh = h + (offset // 3600)
+        nm = m + ((offset % 3600) // 60)
+        ns = s + (offset % 60)
+        
+        # Asegurarse de que la nueva hora, minuto y segundo estén en rangos válidos
+        nh = nh % 24
+        nm = nm % 60
+        ns = ns % 60
+        
+        # Configurar la nueva fecha y hora en el RTC
+        #print(f"P1{y} {m} {d}")
+        rtc.datetime((y, mo, d, 0, nh, nm, ns, 0))
         
     except Exception as ex:
         print("ntpupdate error: ", ex)
