a7c581ef5e648b5a663aa45fb08fb58e6ab8cde2
Rafa <rbenrax@gmil.com>
2023-10-11 18:33:40 +0200

    incr decr

--- bin/decr.py
+++ bin/decr.py
@@ -1,21 +1,30 @@
 import utls
+import sdata
 
 def __main__(args):
 
     if len(args) == 0:
-        print("Decrement a env variable\nUsage: decr <var> [<decr>]:")
+        print("Decrement an env variable\nUsage: decr <var> [<decr>]:")
         return
     
     val=0
     dec=1
     
     if len(args) > 0:
-        if utls.getenv(args[0])=="":
-            utls.setenv(args[0], 0)
+        
+        if not args[0] in sdata.sysconfig["env"]:
+            utls.setenv(args[0], "0")
             
-        val = int(utls.getenv(args[0]))
+        tmp = utls.getenv(args[0])
+        
+        if tmp.lstrip("-").isdigit():
+            val = int(tmp)
+        else:
+            print(f"Variable {args[0]} is not numeric")
+            return
 
     if len(args) == 2:
-        inc=int(args[1])
+        if args[1].isdigit():
+            dec=int(args[1])
 
     utls.setenv(args[0], str(val - dec))
--- bin/incr.py
+++ bin/incr.py
@@ -1,21 +1,30 @@
 import utls
+import sdata
 
 def __main__(args):
 
     if len(args) == 0:
-        print("Increment a env variable\nUsage: incr <var> [<incr>]:")
+        print("Increase an env variable\nUsage: incr <var> [<incr>]:")
         return
     
     val=0
     inc=1
     
     if len(args) > 0:
-        if utls.getenv(args[0])=="":
-            utls.setenv(args[0], 0)
+        
+        if not args[0] in sdata.sysconfig["env"]:
+            utls.setenv(args[0], "0")
             
-        val = int(utls.getenv(args[0]))
+        tmp = utls.getenv(args[0])
+        
+        if tmp.lstrip("-").isdigit():
+            val = int(tmp)
+        else:
+            print(f"Variable {args[0]} is not numeric")
+            return
 
     if len(args) == 2:
-        inc=int(args[1])
+        if args[1].isdigit():
+            inc=int(args[1])
 
     utls.setenv(args[0], str(val + inc))
