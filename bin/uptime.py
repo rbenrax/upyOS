2e379025ebdeeef2125432ec6a6ec9c086e4a045
Rafa <rbenrax@gmail.com>
2023-10-16 09:17:22 +0200

    days up

--- bin/uptime.py
+++ bin/uptime.py
@@ -4,4 +4,4 @@ import sdata
 localt = utime.gmtime(utime.time())
 uptime = utime.gmtime(utime.time() - (sdata.initime))
 
-print (f"{localt[3]:0>2}:{localt[4]:0>2}:{localt[5]:0>2}  {uptime[3]:0>2}:{uptime[4]:0>2}:{uptime[5]:0>2} up")
+print (f"{localt[3]:0>2}:{localt[4]:0>2}:{localt[5]:0>2} {uptime[7]-1:3} days {uptime[3]:0>2}:{uptime[4]:0>2}:{uptime[5]:0>2} up")
