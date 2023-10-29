f25ca40c009c09c1e02b76336e11bb536557b845
Rafa Bena <rbenrax@gmail.com>
2023-10-29 10:16:55 +0100

    new ftpserver

--- bin/uftpd.py
+++ bin/uftpd.py
@@ -1,36 +1,28 @@
 import sys
-import sdata
 
 proc=None
 
 def __main__(args):
 
     if len(args) == 1:
-        #import ftptiny
-        #ftp = ftptiny.FtpTiny()
-        import ftp
+
+        import uftpdserver
         
         dport=21
+        
         if args[0]=="start":
-            #ftp.start()
-            print(f"Starting ftpd service on port {dport}")
-            while not proc.sts=="S":
-                ftp.ftpserver(proc, port=dport, timeout=None)
-                
-        elif args[0]=="stop":
-            #ftp.stop()
-            #del sys.modules["ftptiny"]
-            proc.syscall.run_cmd("killall uftpd")
+            print(f"Starting uftpd service on port {dport}")
+            uftpdserver.start(port=dport, verbose=0)
             
-            #False connection to close de thread
-            import socket
-            sock = socket.socket()
-            sock.connect(("127.0.0.1", dport))
-            sock.close()
-            del sys.modules["ftp"]
+        elif args[0]=="stop":
+            uftpdserver.stop()
+            del sys.modules["uftp"]
+
+        elif args[0]=="restart":
+            uftpdserver.restart()
 
         else:
             print("Invalid argument")
     else:
-        print ("ftptiny, uftpd <options>, start, stop")
+        print ("uftpd, uftpd <options>, start, stop, restart")
 
