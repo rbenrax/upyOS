06e687a3c751489d7fa1c10e0c8aabb76f64b64b
Rafa <rbenrax@gmail.com>
2023-11-13 22:58:33 +0100

    Update rm.py

--- bin/rm.py
+++ bin/rm.py
@@ -3,16 +3,15 @@ import utls
 
 def __main__(args):
 
-    if len(args) != 1:
-        print("Remove file\nUsage: rv <path>")
+    if len(args) == 0 or args[0]="--h":
+        print("Remove file\nUsage: rm <path>")
         return
     else:
-        path=args[0]
-        if path == "": return
-        if utls.protected(path):
-            print("Can not remove system file")
-        else:
-            uos.remove(path)
+        for path in args:
+            if utls.protected(path):
+                print(f"Can not remove system file {path}")
+            else:
+                uos.remove(path)
         
         
 
