c1a1d2fd5cc674fcfe7a892b34acdca6404d9aa5
Rafa <rbenrax@gmail.com>
2023-11-18 15:49:19 +0100

    Update mv.py

--- bin/mv.py
+++ bin/mv.py
@@ -1,38 +1,54 @@
 import uos
 import utls
 
+def mv(sp, dp):
+
+    sf = sp.split("/")[-1]
+    if utls.isdir(dp):
+        if not dp[-1]=="/": dp +="/"
+        dp += sf
+    
+    print(sp + " -> " + dp)
+    with open(sp, 'rb') as fs:
+        with open(dp, "wb") as fd:
+            while True:
+                buf = fs.read(256)
+                if not buf:
+                    break
+                fd.write(buf)
+
+
 def __main__(args):
 
     if len(args) != 2:
-        print("Move file\nUsage: mv <spath> <dpath>")
+        print("Move a file or files in directory\nUsage: mv <spath> <dpath>")
         return
     else:
         spath=args[0]
         dpath=args[1]
         
-    if spath == "" or dpath == "": return
-    
-    if not utls.file_exists(spath):
-        print("File source not exists.")
-        return
-
-    sfile = spath.split("/")[-1]
-    try:
-        uos.listdir(dpath)
-        dpath += "/" + sfile
-    except OSError:
-        pass
-
-    if utls.protected(spath):
-        print("Can not move system files!")
-    else:
-        uos.rename(spath, dpath)
-
-#if __name__ == "__main__":
-
-#    args = ["/main.py", "/tmp"]
-#    __main__(args)
+        if spath == "" or dpath=="": return
         
+        if not utls.file_exists(spath):
+            print("File source not exists.")
+            return
         
-
-        
+        if utls.protected(dpath):
+            print("Can not overwrite system file!")
+        else:
+            if not utls.isdir(spath):
+                mv(spath, dpath)
+            else:
+                if utls.isdir(spath):
+                    if not spath[-1]=="/": spath +="/"
+                    
+                    if utls.isdir(dpath):
+                        if not dpath[-1]=="/": dpath +="/"
+                    
+                    tmp=uos.listdir(spath)
+                    for f in tmp: 
+                        mv(spath+f, dpath)
+                else:
+                    print("not a valid paths " + spath)
+                          
+            
