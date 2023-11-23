efeea0622c83ed7b153ebd48ca07711295424951
Rafa benavides <rbenrax@gmail.com>
2023-11-23 08:27:36 +0000

    .

--- bin/upgrade.py
+++ bin/upgrade.py
@@ -11,12 +11,13 @@ import sdata
 url_raw = f'https://raw.githubusercontent.com/rbenrax/upyOS/main/'
 
 def pull(f_path, url):    
-    #print(f"{f_path} {url} {host} {path}")
+    
     s=None
     ssl_socket=None
     
     try:
         _, _, host, path = url.split('/', 3)
+        #print(f"{f_path} {url} {host} {path}")
         
         s = usocket.socket()
         
@@ -61,7 +62,7 @@ def __main__(args):
 
     mod="" 
     for i in args:
-        if "-" in i: mod+=i[1:]
+        if i[0]=="-": mod +=i [1:]
 
     for p in sdata.procs:
         if p.isthr:
@@ -71,43 +72,41 @@ def __main__(args):
     if "h" in mod:
         print("Upgrade upyOS from git repository\nUsage: upgrade <options>:-f quite mode, -r reboot after upgrade, -v view file list")
         return
+
+    print("upyOS OTA Upgrade, \nDownloading upgrade list...", end="")
+    uf="/etc/upgrade.inf"
+    pull(uf, url_raw + uf[1:])
+    print(", OK")
     
-    else:
-    
-        print("upyOS OTA Upgrade, \nDownloading upgrade list...", end="")
-        uf="/etc/upgrade.inf"
-        pull(uf, url_raw + uf[1:])
-        print(",OK")
+    if not utls.file_exists(uf):
+        print("No upgrade file available, system can not be upgraded")
+        return
         
-        if not utls.file_exists(uf):
-            print("No upgrade file available, system can not be upgraded")
+    if not "f" in mod:
+        r = input("Confirm upyOS upgrade (y/N)? ")
+        if r!="y":
+            print("Upgrade canceled")
             return
+          
+    print("Upgrading from upyOS git repsitory, wait...")
+    print("[", end="")
+    with open(uf, 'r') as f:
+        while True:
+            fp=f.readline()
+            if not fp: break
+            fp=fp[:-1] # remove ending CR
+            if "v" in mod:
+                print(fp, end=", ")
+            else:
+                print(".", end="")
+            pull(fp, url_raw + fp[1:])
             
-        if not "f" in mod:
-            r = input("Confirm upyOS upgrade (y/N)? ")
-            if r!="y":
-                print("Upgrade canceled")
-                return
-              
-        print("Upgrading from upyOS git repsitory, wait...")
-        print("[", end="")
-        with open(uf, 'r') as f:
-            while True:
-                fp=f.readline()
-                if not fp: break
-                fp=fp[:-1] # remove ending CR
-                if "v" in mod:
-                    print(fp, end=", ")
-                else:
-                    print(".", end="")
-                pull(fp, url_raw + fp[1:])
-                
-        os.remove(uf)
-        print("]OK\n100% Upgrade complete")
+    os.remove(uf)
+    print("]OK\n100% Upgrade complete")
+    utime.sleep(2)
+    
+    if "r" in mod:
+        print("Rebooting...")
         utime.sleep(2)
-        
-        if "r" in mod:
-            print("Rebooting...")
-            utime.sleep(2)
-            machine.soft_reset()
+        machine.soft_reset()
 
