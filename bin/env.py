7eeb80d4ca7b99cf07d8b10c5cd282e5a9680039
rafa <rbenrax@gmail.com>
2025-09-15 16:23:02 +0200

    env command fixed

--- bin/env.py
+++ bin/env.py
@@ -2,5 +2,10 @@ import sdata
 
 def __main__(args):
    for k, v in sdata.sysconfig["env"].items():
-       print(f'{k}={v}')
+       #print(f'{k}={v}')
+       if isinstance(v, str):
+           print(f"{k}='{v}'")
+       else:
+           print(f"{k}={v}")
+
                 
