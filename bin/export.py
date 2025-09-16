6ffc897b2ecb7ec613a946aa5309d92a0cb8f89f
Rafael Benavides <rbenavides@rdsistemas.com>
2025-09-16 16:51:03 +0200

    fixed export.py

--- bin/export.py
+++ bin/export.py
@@ -7,7 +7,13 @@ def __main__(args):
 
     if len(args) == 2:
         utls.setenv(args[0], args[1])
+    elif len(args) == 3:
+        if args[2] == 'i':  # Corregido: args[1] -> args[2]
+            utls.setenv(args[0], int(args[1]))  # Corregido: orden de parámetros
+        elif args[2] == 'f':  # Corregido: args[1] -> args[2]
+            utls.setenv(args[0], float(args[1]))  # Corregido: orden de parámetros
+        else:
+            utls.setenv(args[0], args[1])
     else:
-        print("Export env variable\nUsage: export <var>[=]<val>: var ?, 0, 1, $x ..., any")
-
+        print("Export env variable\nUsage: export <var>[=]<val> [i=int, f=float, s=string]: var ?, 0, 1, $x ..., any")
 
