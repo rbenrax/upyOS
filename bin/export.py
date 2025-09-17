e712de839f94feffd6eff2b69daca7a9f7fad852
rafa <rbenrax@gmail.com>
2025-09-17 16:42:38 +0200

    export command bool added

--- bin/export.py
+++ bin/export.py
@@ -1,19 +1,19 @@
 import utls
 
 def __main__(args):
-
-#    if len(args) == 1:
-#        args=args[0].split("=")
-
     if len(args) == 2:
         utls.setenv(args[0], args[1])
     elif len(args) == 3:
-        if args[2] == 'i':  # Corregido: args[1] -> args[2]
-            utls.setenv(args[0], int(args[1]))  # Corregido: orden de parámetros
-        elif args[2] == 'f':  # Corregido: args[1] -> args[2]
-            utls.setenv(args[0], float(args[1]))  # Corregido: orden de parámetros
+        if args[2] == '-i':
+            utls.setenv(args[0], int(args[1]))
+        elif args[2] == '-f':
+            utls.setenv(args[0], float(args[1]))
+        elif args[2] == '-b':
+            if args[1].lower() in ('true', '1', 'on'):
+                utls.setenv(args[0], True)
+            else:
+                utls.setenv(args[0], False)
         else:
             utls.setenv(args[0], args[1])
     else:
-        print("Export env variable\nUsage: export <var>[=]<val> [i=int, f=float, s=string]: var ?, 0, 1, $x ..., any")
-
+        print("Export env variable\nUsage: export <var>[=]<val> [-i=int, -f=float, -s=string, -b=bool]: var ?, 0, 1, $x ..., any")
