a669b41973b3e9b8847ff0920e4521c8ef0153f5
Rafael Benavides <rbenavides@rdsistemas.com>
2025-10-16 16:47:16 +0200

    new Ungrade2.inf

--- lib/sdata.py
+++ lib/sdata.py
@@ -1,22 +1,22 @@
 # Global system data module
 
-name    = ""	# System name
-version = ""	# Current version
-sid     = 0		# System identification
-initime = 0		# Start up time
+name    = "" # System name
+version = "" # Current version
+sid     = 0  # System identification
+initime = 0  # Start up time
 
 # FS cache control (Default false, if speed is required, may be enabled)
 cache_enabled = False
 debug   = True
 log     = False
 
-upyos	= None	# Kernel instance
-board	= {}  	# Board model spec
-sysconfig={}	# System configuration
+upyos = None # Kernel instance
+board = {}   # Board model spec
+sysconfig={} # System configuration
 
 _pid=0
-procs=[]		# Running proccesses
+procs=[] # Running proccesses
 
 # Drivers
-d0 = None		# Global ref to display 0
-m0 = None		# Global ref to modem 0
+d0 = None # Global ref to display 0
+m0 = None # Global ref to modem 0
