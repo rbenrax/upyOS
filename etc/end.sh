8f2a1327cc90e29478893290e17399a0a9b3c22f
Rafael Benavides <rbenavides@rdsistemas.com>
2025-10-16 18:42:14 +0200

    Release 9.0.1

--- etc/end.sh
+++ etc/end.sh
@@ -3,9 +3,3 @@
 test -p uhttpd > 0
 if $0 == True uhttpd stop
 unset 0
-
-wifi sta status -n
-if $wc == uftpd stop
-if $wc == utelnetd stop
-if $wc == True wifi sta disconnect -n
-if $wa == True wifi sta off
