cddd56b7ce0d89459867cd029bc4c0ffcd081664
sivansh11 <sivanshgupta2002@gmail.com>
2025-10-17 10:49:18 +0530

    cd without args goes to /

--- bin/cd.py
+++ bin/cd.py
@@ -1,17 +1,19 @@
 import uos
 
 def __main__(args):
+    if len(args) == 0:
+        uos.chdir('/')
 
-    if len(args) != 1:
-        print("Chage current directory\nUsage: cd <path>")
-        return
-    else:
+    elif len(args) == 1:
         path=args[0]
 
         try:
             uos.chdir(path)
         except OSError:
             print("Invalid path")
+    else:
+        print("Chage current directory\nUsage: cd <path>")
+        return
    
         
 
