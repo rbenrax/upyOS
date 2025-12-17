5f719d8ce3faa2dd3336b2175c222755717bf762
Rafa Bena <rbenrax@gmail.com>
2025-12-17 09:10:23 +0100

    fileup command fixed

--- bin/fileup.py
+++ bin/fileup.py
@@ -1,21 +1,27 @@
 import sdata
 import utls
 
+import sys
+
 def __main__(args):
     
     if len(args) == 0:
-        print ("Upload file in line mode\nUsage: fileup <filename>")
+        print ("Upload file from terminal\nUsage: fileup <filename>")
         return
     
     if utls.protected(args[0]):
         print("Can overwrite system file!")
         return
     
-    print ("Send CTRL+D to end upload")
-    with open(args[0], "wt") as fp:
-      while (True):
-        try:
-            line = input(">")
-            fp.write(line + "\n")
-        except EOFError:
-            break
+    print ("Paste file content and press CTRL+D to save.")
+    
+    try:
+        with open(args[0], "wt") as fp:
+            while True:
+                line = sys.stdin.readline()
+                if not line:
+                    break
+                fp.write(line)
+        print("\nFile saved.")
+    except Exception as e:
+        print(f"Error saving file: {e}")
